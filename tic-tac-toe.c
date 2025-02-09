ac15aec9234774775d3c69762e99de17a7ffe3c4
samTime101 <srijanregmi44@gmail.com>
2025-02-09 14:35:39 +0545

    feat: author and menu

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,7 +1,7 @@
 #include "../../include/raylib.h"
-//#include "../../include/raymath.h"
+// #include "../../include/raymath.h"
 #include <stdio.h>
-//#include <float.h>
+// #include <float.h>
 #include <limits.h>
 #define COLS 3
 #define ROWS 3
@@ -50,13 +50,8 @@ int check_for_draw(int (*board)[3]) {
   }
   return 1;
 }
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
-
+int max(int a, int b) { return (a > b) ? a : b; }
+int min(int a, int b) { return (a < b) ? a : b; }
 
 int minimax(int (*board)[3], int depth, int is_maximizing) {
 
@@ -67,59 +62,56 @@ int minimax(int (*board)[3], int depth, int is_maximizing) {
   if (check_for_draw(board))
     return 0;
 
-  if (is_maximizing){
-    int best_score = INT_MIN; 
-    for (int i = 0 ; i < ROWS ; i++){
-    for (int j = 0 ; j < COLS ; j++){
-      if(board[i][j] == 0){
-        board[i][j] = 1;
-        int score = minimax(board, depth+1, 0);
-        board[i][j] = 0; 
-        best_score = max(score, best_score); 
+  if (is_maximizing) {
+    int best_score = INT_MIN;
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        if (board[i][j] == 0) {
+          board[i][j] = 1;
+          int score = minimax(board, depth + 1, 0);
+          board[i][j] = 0;
+          best_score = max(score, best_score);
         }
-
+      }
     }
-    }
-      return best_score;
-    
+    return best_score;
   }
   int best_score = INT_MAX;
-    for (int i = 0 ; i < ROWS ; i++){
-    for (int j = 0 ; j < COLS ; j++){
-      if(board[i][j] == 0){
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      if (board[i][j] == 0) {
         board[i][j] = -1;
-        int score = minimax(board, depth+1, 1);
-        board[i][j] = 0; 
-        best_score = min(score, best_score); 
-        }
-
+        int score = minimax(board, depth + 1, 1);
+        board[i][j] = 0;
+        best_score = min(score, best_score);
+      }
     }
-    }
+  }
   return best_score;
 }
 
-int *computer_move(int (*board)[3]){
+int *computer_move(int (*board)[3]) {
   int best_score = INT_MIN;
   static int best_move[2];
-  for (int i = 0 ; i < ROWS; i++){
-    for(int j = 0; j < COLS ; j++){
-      if (board[i][j] == 0){
-          board[i][j] = 1;
-          int score = minimax(board, 0, 0);
-          board[i][j] = 0;
-          if (score > best_score){
-              best_score = score;
-              best_move[0] = i;
-              best_move[1] = j;
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      if (board[i][j] == 0) {
+        board[i][j] = 1;
+        int score = minimax(board, 0, 0);
+        board[i][j] = 0;
+        if (score > best_score) {
+          best_score = score;
+          best_move[0] = i;
+          best_move[1] = j;
         }
       }
     }
   }
-  return  best_move;
+  return best_move;
 }
-
-
-
+// REFEREBCE :
+// https://www.raylib.com/examples/core/loader.html?name=core_basic_screen_manager
+typedef enum GameScreen { TITLE = 0, GAMEPLAY, ENDING } GameScreen;
 int main() {
   int mat[ROWS][COLS];
   int player_turn = 1;
@@ -130,64 +122,107 @@ int main() {
       mat[i][j] = 0;
     }
   }
-
+  GameScreen currentScreen = TITLE;
   SetTargetFPS(FPS);
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "AFTER 2 FREAKING YEARS!!");
   while (!WindowShouldClose()) {
-  //  DrawText(hello, SCREEN_HEIGHT/2, SCREEN_WIDTH/2 ,20, GREEN);
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && player_turn) {
-      Vector2 mPos = GetMousePosition();
-      int col = (int)mPos.x / CELL_WIDTH;
-      int row = (int)mPos.y / CELL_HEIGHT;
-      if (row >= 0 && row < ROWS && col >= 0 && col < COLS) {
-
-      if (mat[row][col] == 0) {
-          mat[row][col] = -1;
-          player_turn = 0;
-          computer_turn = 1;
+    switch (currentScreen) {
+    case TITLE: {
+      if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+        currentScreen = GAMEPLAY;
       }
-    }
-    }
-    if (computer_turn){
-      int *target = computer_move(mat);
-      mat[target[0]][target[1]] = 1;
-      player_turn = 1;
-      computer_turn = 0;
-    }
+    } break;
+    case GAMEPLAY: {
+      if (check_for_win(mat, 1) || check_for_win(mat, -1) ||
+          check_for_draw(mat)) {
+        // DrawText("Computer Wins", 200, 200, 10, BLACK);
+        currentScreen = ENDING;
+      }
+      /*
+      if (check_for_win(mat, -1)) {
+        printf("Human wins!\n");
+        // break;
+      }
 
-if (check_for_win(mat, 1)) {
-    //printf("computer wins!\n");
-    DrawText("Computer Wins", 200, 200, 10, BLACK);
-    //break;
-}
-if (check_for_win(mat, -1)) {
-    printf("Human wins!\n");
-    //break;
-}
-
-
-if (check_for_draw(mat)) {
-    printf("Draw!\n");
-    //break;
-}
+      if (check_for_draw(mat)) {
+        printf("Draw!\n");
+        // break;
+      }
+        */
+    } break;
+    default:
+      break;
+    }
+    //  DrawText(hello, SCREEN_HEIGHT/2, SCREEN_WIDTH/2 ,20, GREEN);
     ClearBackground(WHITE);
     BeginDrawing();
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLS; j++) {
-        if (mat[i][j] == -1) {
-          DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
-                        CELL_HEIGHT, BLUE);
-        } else if (mat[i][j] == 1){
-          DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
-                        CELL_HEIGHT, RED);
+    switch (currentScreen) {
+    case TITLE: {
+      DrawText("GAME OF LIFE IN C", 100, 30, 40, DARKGREEN);
+      DrawText("Author : SAMIP REGMI", 100, 80, 30, RED);
+      DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 10, 220, 20,
+               DARKGREEN);
+    } break;
+    case GAMEPLAY: {
+      if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && player_turn) {
+        Vector2 mPos = GetMousePosition();
+        int col = (int)mPos.x / CELL_WIDTH;
+        int row = (int)mPos.y / CELL_HEIGHT;
+        if (row >= 0 && row < ROWS && col >= 0 && col < COLS) {
+
+          if (mat[row][col] == 0) {
+            mat[row][col] = -1;
+            player_turn = 0;
+            computer_turn = 1;
+          }
         }
-        else{
-          DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
-                        CELL_HEIGHT, WHITE);
-        }
-        DrawRectangleLines(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
-                           CELL_HEIGHT, GRAY);
       }
+      if (computer_turn) {
+        int *target = computer_move(mat);
+        mat[target[0]][target[1]] = 1;
+        player_turn = 1;
+        computer_turn = 0;
+      }
+
+      for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+          if (mat[i][j] == -1) {
+            DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
+                          CELL_HEIGHT, BLUE);
+          } else if (mat[i][j] == 1) {
+            DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
+                          CELL_HEIGHT, RED);
+          } else {
+            DrawRectangle(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
+                          CELL_HEIGHT, WHITE);
+          }
+          DrawRectangleLines(j * CELL_WIDTH, i * CELL_HEIGHT, CELL_WIDTH,
+                             CELL_HEIGHT, GRAY);
+        }
+      }
+
+    } break;
+    case ENDING: {
+
+      if (check_for_win(mat, 1)) {
+        DrawText("COMPUTER WON", 100, 30, 40, RED);
+        // break;
+      }break;
+      if (check_for_win(mat, -1)) {
+           DrawText("HUMAN WON", 100, 30, 40, BLUE);
+
+        //printf("Human wins!\n");
+        // break;
+      }break;
+
+      if (check_for_draw(mat)) {
+        //printf("Draw!\n");
+      DrawText("DRAW", 100, 30, 40, BLACK);
+        // break;
+      }
+    }
+    default:
+      break;
     }
 
     EndDrawing();
@@ -195,4 +230,3 @@ if (check_for_draw(mat)) {
   CloseWindow();
   return 0;
 }
-
